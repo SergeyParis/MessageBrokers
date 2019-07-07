22d0bb657976401155dbec94d16da6d8caad3822
sergiyparis <bt1bt1990@gmail.com>
2019-07-07 21:25:56 +0300

    Added comments

--- Microservices-test/Infrastructure/Brokers/RabbitMq/Config/QueueConfig.cs
+++ Microservices-test/Infrastructure/Brokers/RabbitMq/Config/QueueConfig.cs
@@ -2,6 +2,9 @@ namespace Infrastructure.Brokers.RabbitMq.Config
 {
     public class QueueConfig
     {
+        /// <summary>
+        /// Resistant to Rabbit MQ Failures (Save messages to drive)
+        /// </summary>
         public bool IsDurable { get; set; }
     }
 }
