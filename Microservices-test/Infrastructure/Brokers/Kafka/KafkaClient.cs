b367cd8190277ce07fbabef538db5d64bc336955
sergiyparis <bt1bt1990@gmail.com>
2019-07-08 10:05:14 +0300

    Added kafka client

---
+++ Microservices-test/Infrastructure/Brokers/Kafka/KafkaClient.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Infrastructure.Brokers.Kafka
+{
+    public class KafkaClient : IDisposable
+    {
+        public void Dispose()
+        {
+            
+        }
+    }
+}
